e166b4ab5c00199b14f5e850d56f0efa2fbcf7a6
Leonardo Amador <amadorleonardo8109@gmail.com>
2023-08-14 11:16:46 -0300

    Added new http verbs

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const books = [
-  { id: 1, title: "Senhor dos Aneis" },
-  { id: 2, title: "O Hobiit" },
+  { id: 1, "title": "Senhor dos Aneis" },
+  { id: 2, "title": "O Hobiit" },
 ];
 
 app.get("/", (req, res) => {
@@ -14,5 +16,32 @@ app.get("/", (req, res) => {
 app.get('/books', (req, res) => {
     res.status(200).json(books);
 });
+
+app.get('/books/:id', (req, res) => {
+  let index = searchBook(req.params.id);
+  res.json(books[index]);
+});
+
+app.post('/books', (req, res) => {
+  books.push(req.body);
+  res.status(201).send('Book registered successfully')
+});
+
+app.put('/books/:id', (req, res) => {
+  let index = searchBook(req.params.id);
+  
+  index !== -1
+    ? (books[index].title = req.body.title, res.json(books))
+    : res.status(404).send('Book not found');
+});
+
+app.delete('/books/:id', (req, res) => {
+  let { id } = req.params;
+  let index = searchBook(id);
+  books.splice(index, 1);
+  res.send(`Book ${id} removed successfully`);
+});
+
+const searchBook = (id) => books.findIndex(book => book.id === parseInt(id));
 
 export default app;
